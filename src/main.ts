57c14f03cd7141e1417c1694cf5a304348e03a4f
JeffersonSLacerda <jefferson.lacerdasi@outlook.com>
2024-04-18 05:00:57 +0000

    Config documentation with Swagger

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,24 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ConfigService } from '@nestjs/config'
 import { Env } from './env'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  const configService = app.get<ConfigService<Env, true>>(ConfigService)
-  const port = configService.get('PORT', { infer: true })
+  const configDocumentation = new DocumentBuilder()
+    .setTitle('Arena Open Beach Documentation')
+    .setDescription('This is a documentation to backend services from Arena Open Beach App')
+    .setVersion('1.0')
+    .addTag('athlete')
+    .build()
 
+  const document = SwaggerModule.createDocument(app, configDocumentation)
+  SwaggerModule.setup('api', app, document)
+
+  const configService = app.get<ConfigService<Env, true>>(ConfigService)
+
+  const port = configService.get('PORT', { infer: true })
   await app.listen(port)
 }
 bootstrap()

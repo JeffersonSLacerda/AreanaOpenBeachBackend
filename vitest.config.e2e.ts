d330141a96e39cda6e73b7c4b6ee38776d26cdae
JeffersonSLacerda <jefferson.lacerdasi@outlook.com>
2024-04-23 12:13:12 +0000

    Add vitest and config then

---
+++ vitest.config.e2e.ts
@@ -0,0 +1,27 @@
+import swc from 'unplugin-swc';
+import { defineConfig } from 'vitest/config';
+import tsConfiPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  test: {
+    include: ['**/*.e2e-spec.ts'],
+    globals: true,
+    root: './',
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      './src/config/**',
+      '**/data/**',
+    ]
+  },
+  plugins: [
+    tsConfiPaths(),
+    // This is required to build the test files with SWC
+    swc.vite({
+      // Explicitly set the module type to avoid inheriting this value from a `.swcrc` config file
+      module: { type: 'es6' },
+    }),
+  ],
+});

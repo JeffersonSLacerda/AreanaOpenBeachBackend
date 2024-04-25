fb15cfe425278f52532fb9c148153dad0aceb050
JeffersonSLacerda <jefferson.lacerdasi@outlook.com>
2024-04-25 21:15:52 +0000

    Complete read notification test

--- src/domain/notification/application/use-cases/read-notification-use-case.spec.ts
+++ src/domain/notification/application/use-cases/read-notification-use-case.spec.ts
@@ -1,6 +1,7 @@
 import { InMemoryNotificationsRepository } from 'test/repositories/in-memory-notifications-repository'
 import { ReadNotificationUseCase } from './read-notification-use-case'
 import { makeNotification } from 'test/factories/make-notification'
+import { NotAllowedError } from '@/core/errors/types/not-allowed-error'
 
 let inMemoryNotificationsRepository: InMemoryNotificationsRepository
 let sut: ReadNotificationUseCase
@@ -11,7 +12,11 @@ describe('Read Notification', () => {
     sut = new ReadNotificationUseCase(inMemoryNotificationsRepository)
   })
 
-  it('should be ablet to read a new notification', async () => {
+  afterEach(() => {
+    inMemoryNotificationsRepository.clear()
+  })
+
+  it('should be able to read a new notification', async () => {
     const notification = makeNotification()
 
     inMemoryNotificationsRepository.create(notification)
@@ -25,5 +30,20 @@ describe('Read Notification', () => {
     expect(inMemoryNotificationsRepository.items[0].readedAt).toEqual(
       expect.any(Date),
     )
+  })
+
+  it('should not be able to read a other user notification', async () => {
+    const notification = makeNotification()
+
+    inMemoryNotificationsRepository.create(notification)
+
+    const result = await sut.execute({
+      recipientId: '1',
+      notificationId: notification.id.toString(),
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(NotAllowedError)
+    expect(inMemoryNotificationsRepository.items.length).toEqual(1)
   })
 })

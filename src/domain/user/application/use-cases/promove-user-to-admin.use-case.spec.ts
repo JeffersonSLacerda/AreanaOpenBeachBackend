8eaea47c5c818405eed7575c11acedd4d9fbf867
Jefferson Lacerda <47826649+JeffersonSLacerda@users.noreply.github.com>
2024-04-29 23:16:40 +0000

    promove user to admin use case and unit test

---
+++ src/domain/user/application/use-cases/promove-user-to-admin.use-case.spec.ts
@@ -0,0 +1,66 @@
+import { ResourceNotFoundError } from '@/core/errors/types/resource-not-found-error'
+import { InMemoryUsersRepository } from 'test/repositories/in-memory-users-repository'
+import { makeUser } from 'test/factories/make-user'
+import { Role } from '../../enterprise/entities/enums/role'
+import { NotAllowedError } from '@/core/errors/types/not-allowed-error'
+import { PromoveUserToAdminUseCase } from './promove-user-to-admin.use-case'
+
+let usersRepository: InMemoryUsersRepository
+let sut: PromoveUserToAdminUseCase
+describe('Promove user to admin', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new PromoveUserToAdminUseCase(usersRepository)
+  })
+
+  afterEach(() => {
+    usersRepository.clear()
+  })
+
+  it('should be able to promove a user to admin', async () => {
+    const user = makeUser()
+    const editor = makeUser({ role: Role.ADMIN })
+
+    usersRepository.create(user)
+    usersRepository.create(editor)
+
+    const result = await sut.execute({
+      userId: user.id.toString(),
+      editorId: editor.id.toString(),
+    })
+
+    const promovedUser = usersRepository.items.find((item) =>
+      item.id.equals(user.id),
+    )
+
+    expect(result.isRight()).toBe(true)
+    expect(promovedUser).toBeDefined()
+    expect(promovedUser?.role).toBe(Role.ADMIN)
+  })
+
+  it('should not be able, non admin user user, promote user to admin', async () => {
+    const athlete1 = makeUser()
+    const athlete2 = makeUser()
+
+    usersRepository.create(athlete1)
+    usersRepository.create(athlete2)
+
+    const result = await sut.execute({
+      userId: athlete1.id.toString(),
+      editorId: athlete2.id.toString(),
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(NotAllowedError)
+  })
+
+  it('should not be able to promove a inexistent athlete profile', async () => {
+    const result = await sut.execute({
+      userId: '2',
+      editorId: '1',
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).instanceOf(ResourceNotFoundError)
+  })
+})
---
+++ src/domain/user/application/use-cases/promove-user-to-admin.use-case.ts
@@ -0,0 +1,37 @@
+import { Either, left, right } from '@/core/either'
+import { NotAllowedError } from '@/core/errors/types/not-allowed-error'
+import { ResourceNotFoundError } from '@/core/errors/types/resource-not-found-error'
+import { UsersRepository } from '../repositories/users-repository'
+import { Role } from '../../enterprise/entities/enums/role'
+
+interface PromoveUserToAdminUseCaseRequest {
+  userId: string
+  editorId: string
+}
+
+type PromoveUserToAdminUseCaseResponse = Either<
+  ResourceNotFoundError | NotAllowedError,
+  null
+>
+
+export class PromoveUserToAdminUseCase {
+  constructor(private readonly usersRepository: UsersRepository) {}
+
+  async execute({
+    userId,
+    editorId,
+  }: PromoveUserToAdminUseCaseRequest): Promise<PromoveUserToAdminUseCaseResponse> {
+    const userToPromove = await this.usersRepository.findById(userId)
+    const editor = await this.usersRepository.findById(editorId)
+
+    if (!userToPromove || !editor) return left(new ResourceNotFoundError())
+
+    if (editor.role !== Role.ADMIN) return left(new NotAllowedError())
+
+    userToPromove.role = Role.ADMIN
+
+    await this.usersRepository.save(userToPromove)
+
+    return right(null)
+  }
+}
